4c4aea752f5edd455e152838b57a6f25f669f064
arvind021 <ak4392916@gmail.com>
2026-03-27 21:04:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 import re
 from dotenv import load_dotenv
 from pyrogram import filters
-
 load_dotenv()
 
 API_ID = int(os.getenv("API_ID"))
@@ -11,39 +10,34 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID", None))
 OWNER_USERNAME = os.getenv("OWNER_USERNAME", "WTF_WhyMeeh")
 BOT_USERNAME = os.getenv("BOT_USERNAME", "ShrutixMusicBot")
-
 MONGO_DB_URI = os.getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", None))
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")
-
 UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/arvind021/PRE")
 UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = os.getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/+dZCL9T23szs5MzVl")
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/+dZCL9T23szs5MzVl")
-INSTAGRAM = os.getenv("INSTAGRAM", "https://t.me/+dZCL9T23szs5MzVl")
-YOUTUBE = os.getenv("YOUTUBE", "https://t.me/+dZCL9T23szs5MzVl")
-GITHUB = os.getenv("GITHUB", "https://t.me/+dZCL9T23szs5MzVl")
-DONATE = os.getenv("DONATE", "https://t.me/+dZCL9T23szs5MzVl")
+# ✅ Fix: Public links use karo, private invite links nahi
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/ShrutiBots")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/ShrutiSupport")
+INSTAGRAM = os.getenv("INSTAGRAM", "https://instagram.com/")
+YOUTUBE = os.getenv("YOUTUBE", "https://youtube.com/")
+GITHUB = os.getenv("GITHUB", "https://github.com/arvind021/PRE")
+DONATE = os.getenv("DONATE", "https://t.me/ShrutiBots")
 PRIVACY_LINK = os.getenv("PRIVACY_LINK", "https://graph.org/Privacy-Policy-05-01-30")
 
 DURATION_LIMIT_MIN = int(os.getenv("DURATION_LIMIT", 300))
 PLAYLIST_FETCH_LIMIT = int(os.getenv("PLAYLIST_FETCH_LIMIT", 25))
-
 TG_AUDIO_FILESIZE_LIMIT = int(os.getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
 TG_VIDEO_FILESIZE_LIMIT = int(os.getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
-
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET", None)
-
 STRING1 = os.getenv("STRING_SESSION", None)
 STRING2 = os.getenv("STRING_SESSION2", None)
 STRING3 = os.getenv("STRING_SESSION3", None)
 STRING4 = os.getenv("STRING_SESSION4", None)
 STRING5 = os.getenv("STRING_SESSION5", None)
-
 AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
 
 START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/7q8bfg.jpg")
@@ -65,7 +59,6 @@ lyrical = {}
 votemode = {}
 autoclean = []
 confirmer = {}
-
 TEMP_DB_FOLDER = "tempdb"
 
 def time_to_seconds(time):
@@ -80,7 +73,6 @@ if SUPPORT_CHANNEL:
         raise SystemExit(
             "[ERROR] - SUPPORT_CHANNEL URL is invalid. It must start with https://"
         )
-
 if SUPPORT_GROUP:
     if not re.match(r"(?:http|https)://", SUPPORT_GROUP):
         raise SystemExit(
